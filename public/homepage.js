3245cf152351798c90b24af129d2ccd463c13d1b
Aditya Gupta <ag1741923@gmail.com>
2024-09-30 14:18:55 +0530

    formating code

--- public/homepage.js
+++ public/homepage.js
@@ -1,58 +1,65 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
-import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
-import{getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import {
+  getFirestore,
+  getDoc,
+  doc,
+} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCTjtmlU7cFDWm0NakIouvfX6jkTXEOl30",
-    authDomain: "authwithbreastselfexamination.firebaseapp.com",
-    projectId: "authwithbreastselfexamination",
-    storageBucket: "authwithbreastselfexamination.appspot.com",
-    messagingSenderId: "593001136016",
-    appId: "1:593001136016:web:e6e51db5f2b45c3825fd15"
-  };
- 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCTjtmlU7cFDWm0NakIouvfX6jkTXEOl30",
+  authDomain: "authwithbreastselfexamination.firebaseapp.com",
+  projectId: "authwithbreastselfexamination",
+  storageBucket: "authwithbreastselfexamination.appspot.com",
+  messagingSenderId: "593001136016",
+  appId: "1:593001136016:web:e6e51db5f2b45c3825fd15",
+};
 
-  const auth=getAuth();
-  const db=getFirestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  onAuthStateChanged(auth, (user)=>{
-    const loggedInUserId=localStorage.getItem('loggedInUserId');
-    if(loggedInUserId){
-        console.log(user);
-        const docRef = doc(db, "users", loggedInUserId);
-        getDoc(docRef)
-        .then((docSnap)=>{
-            if(docSnap.exists()){
-                const userData=docSnap.data();
-                document.getElementById('loggedUserFName').innerText=userData.firstName;
-                document.getElementById('loggedUserEmail').innerText=userData.email;
-                document.getElementById('loggedUserLName').innerText=userData.lastName;
+const auth = getAuth();
+const db = getFirestore();
 
-            }
-            else{
-                console.log("no document found matching id")
-            }
-        })
-        .catch((error)=>{
-            console.log("Error getting document");
-        })
-    }
-    else{
-        console.log("User Id not Found in Local storage")
-    }
-  })
+onAuthStateChanged(auth, (user) => {
+  const loggedInUserId = localStorage.getItem("loggedInUserId");
+  if (loggedInUserId) {
+    console.log(user);
+    const docRef = doc(db, "users", loggedInUserId);
+    getDoc(docRef)
+      .then((docSnap) => {
+        if (docSnap.exists()) {
+          const userData = docSnap.data();
+          document.getElementById("loggedUserFName").innerText =
+            userData.firstName;
+          document.getElementById("loggedUserEmail").innerText = userData.email;
+          document.getElementById("loggedUserLName").innerText =
+            userData.lastName;
+        } else {
+          console.log("no document found matching id");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document");
+      });
+  } else {
+    console.log("User Id not Found in Local storage");
+  }
+});
 
-  const logoutButton=document.getElementById('logout');
+const logoutButton = document.getElementById("logout");
 
-  logoutButton.addEventListener('click',()=>{
-    localStorage.removeItem('loggedInUserId');
-    signOut(auth)
-    .then(()=>{
-        window.location.href='dashboard.html';
+logoutButton.addEventListener("click", () => {
+  localStorage.removeItem("loggedInUserId");
+  signOut(auth)
+    .then(() => {
+      window.location.href = "dashboard.html";
     })
-    .catch((error)=>{
-        console.error('Error Signing out:', error);
-    })
-  })
+    .catch((error) => {
+      console.error("Error Signing out:", error);
+    });
+});
